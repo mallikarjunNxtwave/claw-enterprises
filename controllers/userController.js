931ca09dcec05ece342078875f2d78e14177ab5c
mallikarjun <mallijangam@gmail.com>
2024-08-02 16:37:03 +0530

    Handling errors in userControllers to send error message

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,7 +18,7 @@ const userRegister = async (request, response) => {
         await newUser.save();
         response.status(200).json({message: "User registerd successfully"});
     } catch (error) {
-        response.status(500).json({error,})
+        response.status(500).json({error: error.message})
     }
 }
 
