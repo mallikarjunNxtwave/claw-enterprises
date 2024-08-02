c9356fc3d1cdc32033e7022ec06f05f59cf2347e
mallikarjun <mallijangam@gmail.com>
2024-08-02 22:01:13 +0530

    Adding error message 'Backend Failes' for testing

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 
 const userRegister = async (request, response) => {
     const {username, email, password} = request.body;
+    console.log(username,email,password);
     try {
         const dbUsername = await User.findOne({username});
         if(dbUsername){
@@ -19,7 +20,7 @@ const userRegister = async (request, response) => {
         await newUser.save();
         response.status(200).json({message: "User registerd successfully"});
     } catch (error) {
-        response.status(500).json({error: error.message})
+        response.status(500).json({error: error.message + "Backend Failed"})
     }
 }
 
