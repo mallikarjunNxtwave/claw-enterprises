133abd8d3126c9dac5a3f1e73a0de4de859ca0f3
mallikarjun <mallijangam@gmail.com>
2024-08-02 23:41:00 +0530

    pusshing again for test

--- routes/userRouts.js
+++ routes/userRouts.js
@@ -7,4 +7,8 @@ router.post('/register', userController.userRegister);
 
 router.post('/login', userController.userLogin);
 
+router.get('/info', (req,res) => {
+    res.json({msg: "Get method"});
+})
+
 module.exports = router;
